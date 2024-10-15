20ef2faeb21c91ff3a2749d6bba3d7d06e581a93
JokingTaremba <jokingtaremba@gmail.com>
2024-10-15 11:53:21 +0200

    feat: Configuração de CORS

--- src/main/java/com/jokingwill/SimpleUserAuthenticationAPI/security/WebSecurityConfiguration.java
+++ src/main/java/com/jokingwill/SimpleUserAuthenticationAPI/security/WebSecurityConfiguration.java
@@ -14,7 +14,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +46,22 @@ public class WebSecurityConfiguration {
                 .userDetailsService(userDetailsServiceImpl)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization",
+                "X-Get-Header", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials"));
+        configuration.setMaxAge(3600L);
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return  source;
     }
 
     @Bean("pass_encoder")
